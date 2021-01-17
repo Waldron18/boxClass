6d90c1addf9f6c4278e84712a9c497db3333780e
Waldron18 <75988657+Waldron18@users.noreply.github.com>
2021-01-17 18:54:42 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,18 @@
+class Ground{
+    constructor(x,y,width,height){
+        var options={
+            isStatic: true
+        }
+        this.ground=Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.ground);
+        this.width=width;
+        this.height=height;
+      
+    }
+
+    display(){
+        fill("blue")
+        rectMode(CENTER);
+        rect(this.ground.position.x,this.ground.position.y,this.width,this.height);
+    }
+}
